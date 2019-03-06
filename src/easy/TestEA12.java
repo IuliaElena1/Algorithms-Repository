37a14dfed3ee5ee57164e464df4585ab21b1b214
IuliaElena1 <silistru.iulia@yahoo.com>
2019-03-06 22:24:30 +0200

    .

--- src/easy/TestEA12.java
+++ src/easy/TestEA12.java
@@ -3,38 +3,38 @@ package easy;
 import java.util.Arrays;
 
 public class TestEA12 {
-	/* Scrieti intr-o clasa numita TestEA12 o metoda care primeste un ca parametru un sir de numere arr si un numar 
-	  * intreg pos ce reprezinta o pozitie din sir si returneaza un sir de dimensiune arr.length - 1 rezultat din 
-	  * eliminarea elementului de pe pozitia pos. Daca pozitia este in afara sirului se va returna null
-	  
-	  removePosition( {5, 8, 11, 3, 6}, 0 ) -> {8, 11, 3, 6}
-	  removePosition( {5, 8, 11, 3, 6}, 4 ) -> {5, 8, 11, 3}
-	  */
+	/*
+	 * Scrieti intr-o clasa numita TestEA12 o metoda care primeste un ca parametru
+	 * un sir de numere arr si un numar intreg pos ce reprezinta o pozitie din sir
+	 * si returneaza un sir de dimensiune arr.length - 1 rezultat din eliminarea
+	 * elementului de pe pozitia pos. Daca pozitia este in afara sirului se va
+	 * returna null
+	 * 
+	 * removePosition( {5, 8, 11, 3, 6}, 0 ) -> {8, 11, 3, 6} removePosition( {5, 8,
+	 * 11, 3, 6}, 4 ) -> {5, 8, 11, 3}
+	 */
 
-	  public static int[] removePosition(int[] arr, int pos) {
-	    int[] result = new int[arr.length - 1];
-	    int indexResult=0;
+	public static int[] removePosition(int[] arr, int pos) {
+		int[] result = new int[arr.length - 1];
+		int indexResult = 0;
 
-	    for (int i = 0; i < result.length+1; i++) {
-	    	
-	    	if( i == pos) {
-	    		continue;}
-    		result[indexResult]=arr[i];
-    		indexResult++;
+		for (int i = 0; i < arr.length; i++) {
 
+			if (i != pos) {
 
-	    }
+				result[indexResult] = arr[i];
+				indexResult++;
 
-	    return result;
+			}
+		}
 
-	  }
+		return result;
 
-	  public static void main(String[] args) {
-
-	    int[] arr = { 5, 8, 11, 3,};
-	   System.out.println(Arrays.toString(removePosition(arr, 3)));
-	  }
 	}
 
+	public static void main(String[] args) {
 
-
+		int[] arr = { 5, 8, 11, 3, 6 };
+		System.out.println(Arrays.toString(removePosition(arr, 2)));
+	}
+}
--- src/easy/TestEA8000.java
+++ src/easy/TestEA8000.java
@@ -1,44 +1,49 @@
 package easy;
 
 public class TestEA8000 {
-	/*Scrieti intr-o clasa numita TestEA8 o metoda care primeste ca parametri doua siruri de 
-	numere a si b si returneaza true daca elementele lui  se regasesc in a in ordinea
-	
-	aparitiei lor si false in caz contrar.
+	/*
+	 * Scrieti intr-o clasa numita TestEA8 o metoda care primeste ca parametri doua
+	 * siruri de numere a si b si returneaza true daca elementele lui se regasesc in
+	 * a in ordinea
+	 * 
+	 * aparitiei lor si false in caz contrar.
+	 * 
+	 * Daca primul sir are lungimea 0 metoda returneaza false. Daca al doilea sir
+	 * are lungimea 0 metoda returneaza true.
+	 */
 
-	Daca primul sir are lungimea 0 metoda returneaza false. Daca al doilea sir are lungimea 0
-	metoda returneaza true.*/
+	// contains({1, 2, 3, 4, 2, 1, 3, 4}=a , {2, 3, 4}=b) -> true
 
-	
-	//contains({1, 2, 3, 4, 2, 1, 3, 4}=a   , {2, 3, 4}=b) -> true
-	 
-	 public static int [] contains(int[] a, int[] b) {
-		 int[] c = new int [b.length];
-		 int indexC=0;
-	 
-		 
-		 for(int i=0; i<a.length ; i++) {		
-			 for(int j=0; j<b.length ; j++) {
-				 if(a[i] == b[j]) {
-					 c[indexC]=b[j];
-					 indexC ++;
-					 
-				 }
-				 
-			 }}
-		 
-		return c;
-		 
-		 
-		 
-		 
+	public static boolean contains(int[] a, int[] b) {
+		int [] c= new int[b.length];
+		int indexC=0;
 
-}
-	 public static void main(String[] args) {
-		 
-		 int [] a ={1, 2, 3, 4, 2, 1, 3, 4};
-		 int [] b= {2, 3, 4};
-		 
-		System.out.println(contains(a,b));
+			for (int j = 0; j < b.length; j++) {
+				for (int i = 0; i < a.length; i++) {
+
+				if (a[i] == b[j]  ) {
+					c[indexC]=b[j];
+					indexC++;}
+					
+					if(c[indexC]==b[j]) {
+						return true;
+					}
+					
+
+				}
+
+			}
+		
+
+		return false;
+
+	}
+
+	public static void main(String[] args) {
+
+		int[] a = { 1, 2, 3, 4, 5, 1, 9, 8 };
+		int[] b = { 2, 4, 3 };
+
+		System.out.println(contains(a, b));
 	}
 }
