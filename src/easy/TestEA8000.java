3605efaddb08efdf8695788b836010cd46c4f037
IuliaElena1 <silistru.iulia@yahoo.com>
2019-03-09 09:12:09 +0200

    .

--- src/easy/TestEA8000.java
+++ src/easy/TestEA8000.java
@@ -12,36 +12,55 @@ public class TestEA8000 {
 	 * are lungimea 0 metoda returneaza true.
 	 */
 
-	// contains({1, 2, 3, 4, 2, 1, 3, 4}=a , {2, 3, 4}=b) -> true
+	// contains({1, 2, 3, 4, 2, 1, 3, 4} = a , {2, 3, 4} = b) -> true
 
 	public static boolean contains(int[] a, int[] b) {
-		int [] c= new int[b.length];
-		int indexC=0;
+		int[] d = containsArray(a, b);
+		int dNumber = numberToArray(d);
+		int bNumber = numberToArray(b);
 
-			for (int j = 0; j < b.length; j++) {
-				for (int i = 0; i < a.length; i++) {
-
-				if (a[i] == b[j]  ) {
-					c[indexC]=b[j];
-					indexC++;}
-					
-					if(c[indexC]==b[j]) {
-						return true;
-					}
-					
-
-				}
-
-			}
-		
+		if (dNumber == bNumber) {
+			return true;
+		}
 
 		return false;
+	}
 
+	public static int numberToArray(int[] array) {
+		int number = array[0];
+
+		for (int i = 1; i < array.length; i++) {
+			number = number * 10 + array[i];
+
+		}
+
+		return number;
+
+	}
+
+	public static int[] containsArray(int[] a, int[] b) {
+		int[] c = new int[b.length];
+		int found = 0;
+		int indexC = 0;
+		for (int j = 0; j < b.length; j++) {
+			for (int i = found; i < a.length; i++) {
+
+				if (a[i] == b[j]) {
+					found = i;
+					c[indexC] = a[found];
+					indexC++;
+					found++;
+
+				}
+			}
+		}
+
+		return c;
 	}
 
 	public static void main(String[] args) {
 
-		int[] a = { 1, 2, 3, 4, 5, 1, 9, 8 };
+		int[] a = { 1, 2, 3, 4, 1, 9, 8 };
 		int[] b = { 2, 4, 3 };
 
 		System.out.println(contains(a, b));
