5fc885c1de46d83185a926fd1d1c7ec66a310dae
IuliaElena1 <silistru.iulia@yahoo.com>
2019-03-09 09:42:26 +0200

    fibonacci

---
+++ src/easy/TestEA10.java
@@ -0,0 +1,32 @@
+package easy;
+
+public class TestEA10 {
+	/*
+	 * Scrieti intr-o clasa numita TestEA10 o metoda care primeste un parametru
+	 * numeric n >= 0 si returneaza al n-lea element din sirul lui Fibonacci. Daca n
+	 * este negativ metoda returneaza -1.
+	 * 
+	 * Sirul lui Fibonacci este dat de urmatoarea formula:
+	 * 
+	 * fibo(0) = 0 fibo(1) = 1 fibo(n) = fibo(n-1) + fibo(n-2) Semnatura metodei
+	 * este:
+	 */
+
+	public static int fibo(int n) {
+
+		if (n == 0) {
+			return 0;
+		}
+
+		if (n == 1) {
+			return 1;
+		}
+
+		return fibo(n - 1) + fibo(n - 2);
+	}
+
+	public static void main(String[] args) {
+
+		System.out.println(fibo(4));
+	}
+}
