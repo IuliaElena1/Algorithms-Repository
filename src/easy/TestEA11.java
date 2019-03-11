b8488e6b9679f875090abb3b18e728b1e9b655e8
IuliaElena1 <44519746+IuliaElena1@users.noreply.github.com>
2019-03-11 14:53:24 +0200

    Update TestEA11.java
    .

--- src/easy/TestEA11.java
+++ src/easy/TestEA11.java
@@ -2,33 +2,34 @@ package easy;
 
 public class TestEA11 {
 
-	  /*Scrieti intr-o clasa numita TestEA11 o metoda care primeste un ca parametru un sir de numere si returneaza 
-	  true daca nu exista elemente duplicate in sir si false in caz contrar.
-	  
-	  Semnatura metodei este:
-	  */
-	  //isUnique( {8, 2, 7, 8, 3, 5} ) -> false
-	  //isUnique( {} ) -> true
+  /*Scrieti intr-o clasa numita TestEA11 o metoda care primeste un ca parametru un sir de numere si returneaza 
+  true daca nu exista elemente duplicate in sir si false in caz contrar.
+  
+  Semnatura metodei este:
+  */
+  //isUnique( {8, 2, 7, 8, 3, 5} ) -> false
+  //isUnique( {} ) -> true
 
-	  public static boolean isUnique(int[] arr) {
+  public static boolean isUnique(int[] arr) {
 
-	    for (int i = 0; i < arr.length; i++) {
+    for (int i = 0; i < arr.length; i++) {
+      for (int j = i + 1; j < arr.length; j++) {
 
-	      if (arr[i] == arr[arr.length - 1 - i]) {
-	        return false;
-	      }
-	    }
+        if (arr[i] == arr[j]) {
+          return true;
+        }
+      }
+    }
 
-	    return true;
+    return false;
 
-	  }
+  }
 
-	  public static void main(String[] args) {
-	    int[] arr = {};
+  public static void main(String[] args) {
+    int[] arr = {};
+    System.out.println(isUnique(arr));
+    System.out.println(isUnique(new int[] { 8, 2, 7, 9, 3, 5 }));
+    System.out.println(isUnique(new int[] { 8, 3, 7, 9, 3, 5 }));
 
-	    System.out.println(isUnique(arr));
-	    System.out.println(isUnique(new int []{8, 2, 7, 8, 3, 5}));
-	    System.out.println(isUnique(new int []{8, 2, 7, 8, 2, 5}));
-
-	  }
+  }
 }
