88aa1b7cba2fbab754f15e6dd651b2acdba5e176
IuliaElena1 <44519746+IuliaElena1@users.noreply.github.com>
2019-03-11 14:33:41 +0200

    Update TestEA9.java
    .

--- src/easy/TestEA9.java
+++ src/easy/TestEA9.java
@@ -17,7 +17,7 @@ public class TestEA9 {
 		
 
 		for (int i = 0; i < arr.length; i++) {
-			for (int j = i ; j < arr.length-1 ; j++) {
+			for (int j = i+1 ; j < arr.length-1 ; j++) {
 				if (arr[i] + arr[j] == n) {
 					count++;
 				}
