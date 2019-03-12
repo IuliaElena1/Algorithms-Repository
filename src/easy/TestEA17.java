c2f17cc1773aec0b25d322d9b586f9694e560eaa
IuliaElena1 <44519746+IuliaElena1@users.noreply.github.com>
2019-03-12 17:51:58 +0200

    Update TestEA17.java
    .

--- src/easy/TestEA17.java
+++ src/easy/TestEA17.java
@@ -3,29 +3,44 @@ package easy;
 import java.util.Arrays;
 
 public class TestEA17 {
-	/*
-	 * Scrieti intr-o clasa numita TestEA17 o metoda care primeste ca parametru un
-	 * sir de numere ordonat crescator, elimina duplicatele si returneaza sirul
-	 * rezultat.
-	 */
+  /*
+  Scrieti intr-o clasa numita TestEA17 o metoda care primeste ca parametru un sir de numere ordonat crescator,
+   elimina duplicatele si returneaza sirul rezultat.
+  
+  Semnatura metodei este:*/
 
-	public static int[] removeDuplicates(int[] arr) {
-		int[] result = new int[arr.length];
-		int indexResult = 0;
+  public static int[] removeDuplicates(int[] arr) {
+    int[] result = new int[arr.length - countDuplicates(arr)];
 
-		for (int i = 0; i < arr.length - 1; i++) {
-			// for (int j = 1; j < arr.length - 1; j++) {
-			if (arr[i] != arr[i + 1]) {
-				result[indexResult] = arr[i];
-				indexResult++;
-			}
-		}
+    int index = 0;
+    // result[index] = arr[0];
 
-		return result;
+    for (int i = 0; i < arr.length - 1; i++) {
 
-	}
+      if (arr[i] < arr[i + 1]) {
+        result[index] = arr[i];
+        index++;
+      }
 
-	public static void main(String[] args) {
-		System.out.println(Arrays.toString(removeDuplicates(new int[] { 1, 1, 2, 2, 2, 3, 3, 3, 3 })));
-	}
+    }
+    return result;
+
+  }
+
+  public static int countDuplicates(int[] arr) {
+    int count = 0;
+    for (int i = 0; i < arr.length - 1; i++) {
+      if (arr[i] == arr[i + 1]) {
+        count++;
+      }
+    }
+    return count;
+
+  }
+
+  public static void main(String[] args) {
+    System.out.println(Arrays.toString(removeDuplicates(new int[] { 1, 1, 2, 2, 3, 3, 3, 3 })));
+    System.out.println("Count duplicates= " + countDuplicates(new int[] { 1, 1, 2, 2, 3, 3, 3, 3 }));
+  }
+
 }
