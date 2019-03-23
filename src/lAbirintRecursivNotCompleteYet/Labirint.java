82395f2da5b8c02226df86cdfb769b3605e5712e
IuliaElena1 <silistru.iulia@yahoo.com>
2019-03-23 08:11:13 +0200

    .

--- src/lAbirintRecursivNotCompleteYet/Labirint.java
+++ src/lAbirintRecursivNotCompleteYet/Labirint.java
@@ -1,4 +1,4 @@
-package labirintRecursiv;
+package lAbirintRecursivNotCompleteYet;
 
 import java.util.ArrayList;
 import java.util.List;
--- src/lAbirintRecursivNotCompleteYet/Point.java
+++ src/lAbirintRecursivNotCompleteYet/Point.java
@@ -1,4 +1,4 @@
-package labirintRecursiv;
+package lAbirintRecursivNotCompleteYet;
 
 public class Point {
 
